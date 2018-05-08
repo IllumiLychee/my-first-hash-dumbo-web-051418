02896a4eaa60388d47a50da88267974108497ad3
Angelina Wong <angelina.w25@gmail.com>
2018-05-08 22:21:45 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  your_hash = {"dog" => "lychee", "kid" = "too many", "food" => "Unlimited"}
+  pets = {"cat" => "Maru", "dog" => "Pluto"}
 
 end
 
